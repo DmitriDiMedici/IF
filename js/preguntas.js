14f29d126a42334b1a7d7217b224acc6eb72e776
DmitriDiMedici <84802823+DmitriDiMedici@users.noreply.github.com>
2023-11-22 00:21:47 -0600

    Add files via upload

---
+++ js/preguntas.js
@@ -0,0 +1,57 @@
+let questions = [
+  {
+    numb: 1,
+    question: "¿Qué es el alquiler?",
+    answer: "El gasto de pagar la renta de una casa",
+    options: [
+      "La persona que alquila un lugar",
+      "Pedir prestado un lugar para vivir",
+      "El gasto de pagar la renta de una casa",
+      "Ser el dueño de una casa y rentarla",
+    ],
+  },
+  {
+    numb: 2,
+    question: "¿A qué se refiere el ocio en cuanto a dinero?",
+    answer: "Gastos en diversión y cosas no productivas",
+    options: [
+      "La madre de todos los vicios",
+      "Gastos en actividades de diversión y cosas no productivas",
+      "No generar ingresos por andar tirando flojera",
+      "Comprar cosas que no te hacen faltas",
+    ],
+  },
+  {
+    numb: 3,
+    question: "¿Qué es el ahorro?",
+    answer: "Acción de guardar dinero para después",
+    options: [
+      "Gastar dinero en cosas que más adelante nos darán más dinero",
+      "No gastar dinero nunca",
+      "Ser tacaño",
+      "Acción de guardar dinero para después",
+    ],
+  },
+  {
+    numb: 4,
+    question: "¿Qué es el presupuesto?",
+    answer: "Dinero guardado con un propósito en específico",
+    options: [
+      "Dinero guardado con un propósito en específico",
+      "Dinero que tienes como respaldo en caso de emergencias",
+      "Dinero que escondes de tu familia para que no te lo roben",
+      "Dinero que le debes al banco",
+    ],
+  },
+  {
+    numb: 5,
+    question: "¿Qué son las deudas?",
+    answer: "Dinero que se le debe a algo o alguien",
+    options: [
+      "Dinero que tienes como respaldo en caso de emergencias",
+      "Dinero que vas a gastar en algo divertido",
+      "Dinero que se le debe a algo o alguien",
+      "Dinero que escondes de tu familia para que no te lo roben",
+    ],
+  },
+];
